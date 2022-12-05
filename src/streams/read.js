d5fdaaa26f142892d8b7a49a14de9e3b0e7410ae
SPECTRA995 <maxmillerasce@gmail.com>
2022-12-06 02:13:36 +0300

    fix: upgrade bad code

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,26 +1,14 @@
-// import fs from 'fs';
-
-// const read = async () => {
-//     const readStream = fs.createReadStream('/src/streams/files/fileToRead.txt');
-//     readStream.pipe(process.stdout);
-// };
-
-// await read();
-
-
-
-import {createReadStream} from 'node:fs';
-import {dirname} from 'node:path';
+import fs from 'fs';
+import path from 'path';
 import {fileURLToPath} from 'node:url';
 import {pipeline} from 'node:stream/promises';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const targetFile = `${__dirname}/files/fileToRead.txt`
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const file = `${dirName}/files/fileToRead.txt`;
 
 const read = async () => {
-    const fileStream = createReadStream(targetFile);
-
-    await pipeline(fileStream, process.stdin);
+    const readStream = fs.createReadStream(file);
+    pipeline(readStream, process.stdin);
 };
 
 await read();

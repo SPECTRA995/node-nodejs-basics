81a5f8319d30198d017c7895db2010adf3c9bde8
SPECTRA995 <maxmillerasce@gmail.com>
2022-12-06 02:03:42 +0300

    feat: do calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fsPromises from 'fs/promises'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const getText = await fsPromises.readFile(
+        path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+    );
+    const hash = createHash('sha256').update(getText).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,8 +1,26 @@
-import fs from 'fs';
+// import fs from 'fs';
+
+// const read = async () => {
+//     const readStream = fs.createReadStream('/src/streams/files/fileToRead.txt');
+//     readStream.pipe(process.stdout);
+// };
+
+// await read();
+
+
+
+import {createReadStream} from 'node:fs';
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {pipeline} from 'node:stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const targetFile = `${__dirname}/files/fileToRead.txt`
 
 const read = async () => {
-    const readStream = fs.createReadStream('/src/streams/files/fileToRead.txt');
-    readStream.pipe(process.stdout);
+    const fileStream = createReadStream(targetFile);
+
+    await pipeline(fileStream, process.stdin);
 };
 
 await read();

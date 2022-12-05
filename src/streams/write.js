ec97f956bba7bd469980832dbf979a827b249c52
SPECTRA995 <maxmillerasce@gmail.com>
2022-12-06 01:08:31 +0300

    feat: do stream/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream('/src/streams/files/fileToWrite.txt');
+    writeStream.on('err', (err) => console.log(err));
+    
+    process.stdin.on('data', (data) => {
+        writeStream.write(data);
+        writeStream.close();
+        process.exit();
+    });
 };
 
 await write();

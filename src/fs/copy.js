255ae8e1af26c2c104184c2d2a434aea7374c24e
SPECTRA995 <maxmillerasce@gmail.com>
2022-12-06 00:59:04 +0300

    feat: do copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    fs.readdir('src/fs/files', (err, files) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            fs.mkdir('src/fs/files_copy', (err) => {
+                if (err) {
+                    throw new Error('FS operation failed');
+                }
+                else {
+                    files.forEach((file) => {
+                        fs.copyFile(`src/fs/files/${file}`, `src/fs/files_copy/${file}`, (err) => {
+                            if (err) {
+                                throw new Error('FS operation failed');
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });
 };
 
 await copy();
